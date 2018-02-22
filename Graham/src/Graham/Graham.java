c815f86df6181cfe9a9770fe028b6bf389b8bcb0
Aaron <aburn02s@t-800.socialwork.illinois.edu>
2018-02-22 16:51:04 -0600

    Adds basic workflow comments.

--- Graham/src/Graham/Graham.java
+++ Graham/src/Graham/Graham.java
@@ -3,7 +3,18 @@ package Graham;
 public class Graham {
 
     public static void main(String[] args) {
-        // TODO code application logic here
+        // Read config file which should contain (this should be its own class):
+            // VM name
+            // Disk name
+            // Disk path
+        
+        // Create LVM Snapshot
+        
+        // Copy snapshot to disk image
+        
+        // Backup Verification
+        
+        // If Verification succeeds, remove snapshot
     }
     
 }
