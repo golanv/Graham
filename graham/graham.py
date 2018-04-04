ef63f4b357d480f98298c5ff9a6b4f7a2bd82814
Aaron <aaron@adelie.io>
2018-04-04 09:58:41 -0500

    Initial graham.py

---
+++ graham/graham.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+from classes import *
+import sys
+
+def main(args):
+    #print(args)
+    destPath = None
+    didks = []
+    pool = None
+    
+    
+    
+if __name__ == '__main__':
+    main(sys.argv)
